61cdb2a982a7a418df9a7ba79ac2321f6fb50fa9
Chris Ahrendt <chrisahrendt@bellsouth.net>
2026-05-23 22:13:40 -0400

    Delete src/cb_analytics directory

--- src/cb_analytics/observability/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2026 Chris Ahrendt
-# SPDX-License-Identifier: MIT
-"""Observability: Prometheus metrics registry."""
-from cb_analytics.observability.metrics import MetricsRegistry
-__all__ = ["MetricsRegistry"]
